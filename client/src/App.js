edcef5c9b04462a6e9fa0c24bacb82bcf1b6f338
Mikael Ojaskivi <mikaelojaskivi@Mikaels-Work-MacBook-Pro.local>
2020-01-14 13:24:16 +0100

    added styled component

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,52 @@
 import React from 'react';
 import logo from './images/spacex-logo.png';
 import { Route, Switch } from 'react-router-dom';
-
-import './App.css';
+import styled from 'styled-components';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import CapsulesList from './components/CapsulesList';
 
+const AppContainer = styled.div`
+    text-align: center;
+`;
+
+const AppHeader = styled.header`
+    background-color: #37393a;
+    padding-top: 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+
+const AppLogo = styled.img`
+    width: 70%;
+    pointer-events: none;
+`;
+
+const SubHeading = styled.p`
+
+`;
+
 function App() {
     return (
         <main>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="Spacex logo" />
-                    <p>
-                        <code>Playground</code>
-                        <br />
-                    </p>
-                </header>
+            <AppContainer>
+                <AppHeader>
+                    <AppLogo src={logo} alt="Spacex logo" />
+                    <SubHeading><code>Playground</code></SubHeading>
+                </AppHeader>
                 <Navbar />
 
                 <Switch>
                     <Route path="/" component={Home} exact />
                     <Route path="/capsules" component={CapsulesList} />
                 </Switch>
-                </div>
-            </main>
+            </AppContainer>
+        </main>
     );
 }
 
