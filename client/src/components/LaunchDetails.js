1d153c6dd6863c32278dbe946ebd8d24a908eef0
Mikael Ojaskivi <mikaelojaskivi@Mikaels-Work-MacBook-Pro.local>
2020-01-16 15:30:59 +0100

    commit before bigger changes

--- client/src/components/LaunchDetails.js
+++ client/src/components/LaunchDetails.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import LoaderSpinner from './LoaderSpinner';
+import LaunchDetailsContent from './LaunchDetailsContent';
 import LinkIcon from './common/LinkIcon';
 import LinkRow from './common/LinkRow';
 import ThumbnailGrid from './common/ThumbnailGrid';
@@ -9,11 +10,10 @@ import YoutubeVideo from './YoutubeVideo';
 
 import defaultMissionPatch from '../images/spacex-x.png';
 
-
 const MissionPatch = styled.img`
     max-width: 350px;
     margin: 25px auto;
-    margin-right: ${props => props.placeholderPatch ? "-175px" : "0"};
+    margin-right: ${props => (props.placeholderPatch ? '-175px' : '0')};
 `;
 
 function LaunchDetails(props) {
@@ -40,17 +40,18 @@ function LaunchDetails(props) {
             {isLoading ? <LoaderSpinner /> : ''}
             {launch.links?.mission_patch ? <MissionPatch src={launch.links?.mission_patch} /> : <MissionPatch src={defaultMissionPatch} placeholderPatch />}
             <p>{launch.mission_name}</p>
-            <LinkRow links={[
-                launch.links?.article_link ? <LinkIcon key={launch.links?.article_link} href={launch.links?.article_link} linkType='article' /> : '',
-                launch.links?.wikipedia ? <LinkIcon key={launch.links?.wikipedia} href={launch.links?.wikipedia} linkType='wikipedia' /> : '',
-                launch.links?.video_link ? <LinkIcon key={launch.links?.video_link} href={launch.links?.video_link} linkType='youtube'/> : '',
-                launch.links?.presskit ? <LinkIcon key={launch.links?.presskit} href={launch.links?.presskit} linkType='presskit' /> : '',
-            ]} />
+            <LinkRow
+                links={[
+                    launch.links?.article_link ? <LinkIcon key={launch.links?.article_link} href={launch.links?.article_link} linkType="article" /> : '',
+                    launch.links?.wikipedia ? <LinkIcon key={launch.links?.wikipedia} href={launch.links?.wikipedia} linkType="wikipedia" /> : '',
+                    launch.links?.video_link ? <LinkIcon key={launch.links?.video_link} href={launch.links?.video_link} linkType="youtube" /> : '',
+                    launch.links?.presskit ? <LinkIcon key={launch.links?.presskit} href={launch.links?.presskit} linkType="presskit" /> : '',
+                ]}
+            />
 
             <p>{launch.launch_success ? 'Successful launch' : 'Failed launch: ' + launch.launch_failure_details?.reason}</p>
 
-
-<h2>Media:</h2>
+            <h2>Media:</h2>
             <YoutubeVideo videoId={launch.links?.youtube_id} />
 
             {waitingForImages ? <LoaderSpinner /> : <ThumbnailGrid images={launch.links?.flickr_images} />}
---
+++ client/src/components/LaunchDetailsContent.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+import LoaderSpinner from './LoaderSpinner';
+import LinkIcon from './common/LinkIcon';
+import LinkRow from './common/LinkRow';
+import ThumbnailGrid from './common/ThumbnailGrid';
+import YoutubeVideo from './YoutubeVideo';
+
+import defaultMissionPatch from '../images/spacex-x.png';
+
+const MissionPatch = styled.img`
+    max-width: 350px;
+    margin: 25px auto;
+    margin-right: ${props => (props.placeholderPatch ? '-175px' : '0')};
+`;
+
+function LaunchDetailsContent(props) {
+    const [isLoading, setIsLoading] = useState(true);
+    const [waitingForImages, setWaitingForImages] = useState(true);
+    
+    return (
+        <div>
+            {console.log('props: LaunchDetailsContent', props)}
+            {props.launch.links?.mission_patch ? <MissionPatch src={props.launch.links?.mission_patch} /> : <MissionPatch src={defaultMissionPatch} placeholderPatch />}
+            <p>{props.launch.mission_name}</p>
+            <LinkRow
+                links={[
+                    props.launch.links?.article_link ? <LinkIcon key={props.launch.links?.article_link} href={props.launch.links?.article_link} linkType="article" /> : '',
+                    props.launch.links?.wikipedia ? <LinkIcon key={props.launch.links?.wikipedia} href={props.launch.links?.wikipedia} linkType="wikipedia" /> : '',
+                    props.launch.links?.video_link ? <LinkIcon key={props.launch.links?.video_link} href={props.launch.links?.video_link} linkType="youtube" /> : '',
+                    props.launch.links?.presskit ? <LinkIcon key={props.launch.links?.presskit} href={props.launch.links?.presskit} linkType="presskit" /> : '',
+                ]}
+            />
+
+            <p>{props.launch.launch_success ? 'Successful launch' : 'Failed launch: ' + props.launch.launch_failure_details?.reason}</p>
+
+            <h2>Media:</h2>
+            <YoutubeVideo videoId={props.launch.links?.youtube_id} />
+
+            {waitingForImages ? <LoaderSpinner /> : <ThumbnailGrid images={props.launch.links?.flickr_images} />}
+        </div>
+    );
+}
+
+export default LaunchDetailsContent;
